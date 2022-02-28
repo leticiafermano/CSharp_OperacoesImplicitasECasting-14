c8b4219b9c1a842a8c6969f7177cd61bd9c8d4b3
Lethícia Santos <leticia-santos75@hotmail.com>
2022-02-28 15:12:29 -0300

    Operações Implicitas e Casting.

--- CSharp_OperacoesImplicitasECasting/Program.cs
+++ CSharp_OperacoesImplicitasECasting/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 
-namespace CSharp_OperacoesImplicitasECasting
+namespace CSharp_ConversaoImplicitaeCasting
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* Conversão implicita */
+            float a = 4.1f; // Float = 4 bits
+            double b = a; // Double = 8 bits
+            Console.WriteLine(b);
+
+
+            /* Casting - Conversão explicita */
+            a = 5.4f;
+            b = (float)a;
+            Console.WriteLine(b);
+
+
+            /* Nesse caso abaixo, haverá perda de informação devido ao truncamento */
+            double c = 5.9;
+            int d = (int)a;
+            Console.WriteLine(d);
+
+
+            int x = 5;
+            int y = 2;
+
+            double resultado = (double)x / y;
+            Console.WriteLine(resultado);
         }
     }
 }
