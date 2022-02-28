a8a5634652270d21f8c3efea0a071455c088aa4b
Lethícia Santos <leticia-santos75@hotmail.com>
2022-02-28 15:11:59 -0300

    Adicionar arquivos de projeto.

---
+++ CSharp_OperacoesImplicitasECasting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_OperacoesImplicitasECasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
